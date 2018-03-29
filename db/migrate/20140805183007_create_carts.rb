edc94f68716f59020dd08095dea98474db76d59e
tpetersen0308 <travis.e.petersen@gmail.com>
2018-03-29 14:51:18 -0700

    Done.

--- db/migrate/20140805183007_create_carts.rb
+++ db/migrate/20140805183007_create_carts.rb
@@ -1,5 +1,8 @@
 class CreateCarts < ActiveRecord::Migration
   def change
-
+    create_table :carts do |t|
+      t.decimal :total, :precision => 8, :scale => 2
+      t.timestamps null: false
+    end
   end
 end
--- db/migrate/20140805183012_create_items.rb
+++ db/migrate/20140805183012_create_items.rb
@@ -1,5 +1,9 @@
 class CreateItems < ActiveRecord::Migration
   def change
-
+    create_table :items do |t|
+      t.integer :category_id
+      t.decimal :price, :precision => 8, :scale => 2
+      t.timestamps null: false
+    end
   end
 end
