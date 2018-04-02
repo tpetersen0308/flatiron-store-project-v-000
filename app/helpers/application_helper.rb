5b0164d65ed04647b3fea0a4efbaab209a0ea617
tpetersen0308 <travis.e.petersen@gmail.com>
2018-04-02 15:28:20 -0700

    Done.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
   def logged_in?
     current_user ? true : false
   end
+
+  def nav_header
+    nav_html = content_tag(:li, link_to("Store Home", store_path))
+    if logged_in?
+      if !!@cart
+        nav_html += content_tag(:li, link_to("Cart", cart_path(current_user.current_cart)))
+      end
+      nav_html += content_tag(:li, link_to("Sign out", destroy_user_session_path, method: 'delete'))
+    else
+      nav_html += (content_tag(:li, link_to("Sign up", new_user_registration_path)) +
+      content_tag(:li, link_to("sign in", new_user_session_path)))
+    end
+  end
+
 end
